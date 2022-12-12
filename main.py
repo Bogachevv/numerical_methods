13a82f22c965f2c3fa980a5f421625804e96f955
Bogachevv <dog67688@gmail.com>
2022-12-13 00:35:16 +0300

    Add 'stability matrix'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import typing
 from matplotlib import pyplot as plt
+import scipy.integrate as integrate
 
 
 def interpolate(net: np.ndarray, f: np.ndarray) -> typing.Callable:
@@ -35,22 +36,34 @@ def solve_gauss(a: np.matrix, f: np.ndarray) -> np.ndarray:
 def approach(net: np.ndarray, f: np.array, n: int):
     A = np.matrix([[nd ** j for j in range(n + 1)] for nd in net])
 
-    # DEBUG: delete in release version
-    alpha = np.linalg.solve(A.transpose() * A, (f * A).transpose())
-    np.set_printoptions(precision=4, threshold=5, edgeitems=4, suppress=True)
-    alpha = alpha.A1
-    print(f"alpha = {alpha}")
-    print(f"{np.linalg.cond(A.transpose() * A)=}")
-    print(f"{np.linalg.det(A.transpose() * A)=}")
-    # !DEBUG
-
     alpha_gauss = solve_gauss(A.transpose() * A, (f * A).transpose())
-    print(f"alpha_gauss = {alpha_gauss}")
+    # print(f"alpha_gauss = {alpha_gauss}")
 
     def pol(x: float | np.ndarray) -> float | np.ndarray:
-        return sum(ai * (x ** i) for i, ai in enumerate(alpha))
+        return sum(ai * (x ** i) for i, ai in enumerate(alpha_gauss))
 
     return pol
+
+
+def get_st_elm(x_net: np.ndarray, f_net, i, j, method: str, n: int = 0) -> float:
+    mask = np.full(x_net.shape, True)
+    mask[i] = False
+    x_net_i = x_net[mask]
+    f_net_i = f_net[mask]
+    mask[i] = True
+    mask[j] = False
+    x_net_j = x_net[mask]
+    f_net_j = f_net[mask]
+    mask[j] = True
+
+    if method == "interpolate":
+        f_i = interpolate(x_net_i, f_net_i)
+        f_j = interpolate(x_net_j, f_net_j)
+    else:
+        f_i = approach(x_net_i, f_net_i, n)
+        f_j = approach(x_net_j, f_net_j, n)
+
+    return integrate.quad(lambda x: (f_i(x) - f_j(x)) ** 2, min(x_net), max(x_net))[0]
 
 
 def main():
@@ -77,6 +90,26 @@ def main():
     plt.legend(["actual", "interpolate", "approach"])
     plt.show()
     # <<!-summary graph-!>>
+
+    m_st_interpolate = np.array([[get_st_elm(x_net, f_net, i, j, 'interpolate', N)
+                                  for j in range(len(x_net))] for i in range(len(x_net))])
+
+    m_st_approach = np.array([[get_st_elm(x_net, f_net, i, j, 'approach', N)
+                               for j in range(len(x_net))] for i in range(len(x_net))])
+
+    print()
+    for i in range(len(x_net)):
+        for j in range(len(x_net)):
+            print(f"{m_st_interpolate[i, j]:09.3f}", end='\t')
+        print()
+
+    print()
+    for i in range(len(x_net)):
+        for j in range(len(x_net)):
+            print(f"{m_st_approach[i, j]:09.3f}", end='\t')
+        print()
+
+    print(f"{np.mean(m_st_interpolate)=}\n{np.mean(m_st_approach)=}")
 
 
 if __name__ == '__main__':
